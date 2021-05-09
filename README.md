a589704d971b9a3723e36827307dcc267343901b
rhwu <p98943001@gmail.com>
2021-05-10 06:16:40 +0800

    Successfully get vesdio & using dotenv to setup env

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # youtube-concatenate
 this is my first project using github
-using token 
-test github 
+1. Get channel id by : https://socialnewsify.com/get-channel-id-by-username-youtube/
+2. Code comes from : https://socialnewsify.com/get-channel-id-by-username-youtube/
--- youtube_concatenate/main.py
+++ youtube_concatenate/main.py
@@ -1,39 +1,22 @@
-# Get channel id by : https://socialnewsify.com/get-channel-id-by-username-youtube/
-# Code comes from : https://socialnewsify.com/get-channel-id-by-username-youtube/
-
-
-import urllib.request
-import json
-from youtube_concatenate.settings import API_KEY
-
-print(API_KEY)
+from youtube_concatenate.pipeline.steps.get_vedio_list import GetVedioList
+from youtube_concatenate.pipeline.steps.step import StepException
+from youtube_concatenate.pipeline.pipeline import Pipeline
 
 CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
 
 
-def get_all_video_in_channel(channel_id):
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID,
+    }
 
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
-                                                                                                        channel_id)
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
+    steps = [
+        GetVedioList(),
+    ]
 
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-vedio_list = get_all_video_in_channel(CHANNEL_ID)
-print(len(vedio_list))
+if __name__ == '__main__':
+    main()
